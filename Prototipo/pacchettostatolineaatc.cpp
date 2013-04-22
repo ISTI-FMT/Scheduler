dccb947eb5926b4dfd1613e0978b0e4a660f52ba
Giorgio <imatesiu@gmail.com>
2013-04-22 15:59:28 +0200

    finite strutture per trattare i messaggi di stato della linea ricevuto di ATC

--- Prototipo/pacchettostatolineaatc.cpp
+++ Prototipo/pacchettostatolineaatc.cpp
@@ -16,3 +16,110 @@ pacchettostatolineaatc::pacchettostatolineaatc(void)
 	data.pkg.pstato.Q_DEVIATIOIO = 0;
 	data.pkg.pstato1 = NULL;
 }
+
+// funzione che sette N_ITER1
+void pacchettostatolineaatc::setN_ITER(int N)
+{
+	data.pkg.N_ITER = N;
+	data.pkg.pstato1 = new pstatolineastruct[N];
+}
+
+void pacchettostatolineaatc::setNID_CDB(int index, int D)
+{
+	if(index == 0)
+		data.pkg.pstato.NID_CDB = D;
+	else
+		data.pkg.pstato1[index - 1].NID_CDB  = D;
+}
+
+int pacchettostatolineaatc::getNID_CDB(int index)
+{
+	if(index == 0)
+		return data.pkg.pstato.NID_CDB;
+	else
+		return data.pkg.pstato1[index - 1].NID_CDB;
+}
+
+void pacchettostatolineaatc::setQ_STATOCDB(int index, int D)
+{
+	if(index == 0)
+		data.pkg.pstato.Q_STATOCDB = D;
+	else
+		data.pkg.pstato1[index - 1].Q_STATOCDB  = D;
+}
+
+int pacchettostatolineaatc::getQ_STATOCDB(int index)
+{
+	if(index == 0)
+		return data.pkg.pstato.Q_STATOCDB;
+	else
+		return data.pkg.pstato1[index - 1].Q_STATOCDB;
+}
+
+void pacchettostatolineaatc::setQ_DEVIATIOIO(int index, int D)
+{
+	if(index == 0)
+		data.pkg.pstato.Q_DEVIATIOIO = D;
+	else
+		data.pkg.pstato1[index - 1].Q_DEVIATIOIO  = D;
+}
+
+int pacchettostatolineaatc::getQ_DEVIATIOIO(int index)
+{
+	if(index == 0)
+		return data.pkg.pstato.Q_DEVIATIOIO;
+	else
+		return data.pkg.pstato1[index - 1].Q_DEVIATIOIO;
+}
+
+
+void pacchettostatolineaatc::serialize(byte *buffer)
+{
+	push(buffer, data.head.NID_MESSAGE, 8, 0);
+	push(buffer, data.head.L_MESSAGE, 11, 8);
+	push(buffer, data.head.T_TRAIN, 32, 19);
+	push(buffer, data.pkg.NID_PACKET, 8, 51);
+	push(buffer, data.pkg.L_PACKET, 13, 59);
+	push(buffer, data.pkg.NID_OPERATIONAL, 32, 91);
+	push(buffer, data.pkg.pstato.NID_CDB, 32, 123);
+	push(buffer, data.pkg.pstato.Q_STATOCDB, 2, 155);
+    push(buffer, data.pkg.pstato.Q_DEVIATIOIO, 2, 157);
+	push(buffer, data.pkg.N_ITER, 5, 162);
+	int offset = 167;
+	for(unsigned int i = 0; i < data.pkg.N_ITER; ++i)
+	{
+		push(buffer, data.pkg.pstato1[i].NID_CDB, 32, offset);
+		offset += 32;
+		push(buffer, data.pkg.pstato1[i].Q_STATOCDB, 2, offset);
+		offset += 2;
+		push(buffer, data.pkg.pstato1[i].Q_DEVIATIOIO, 2, offset);
+		offset += 2;
+	}
+	
+}
+
+void pacchettostatolineaatc::deserialize(byte *buffer)
+{
+	data.head.NID_MESSAGE=pop(buffer, 8, 0);
+	data.head.L_MESSAGE=pop(buffer, 11, 8);
+	data.head.T_TRAIN=pop(buffer, 32, 19);
+	data.pkg.NID_PACKET=pop(buffer,  8, 51);
+	data.pkg.L_PACKET=pop(buffer, 13, 59);
+	data.pkg.NID_OPERATIONAL=pop(buffer, 32, 91);
+	data.pkg.pstato.NID_CDB=pop(buffer, 32, 123);
+	data.pkg.pstato.Q_STATOCDB=pop(buffer, 2, 155);
+	data.pkg.pstato.Q_DEVIATIOIO=pop(buffer, 2, 157);
+	data.pkg.N_ITER=pop(buffer, 5, 162);
+	int offset = 167;
+	for(unsigned int i = 0; i < data.pkg.N_ITER; ++i)
+	{
+		data.pkg.pstato1[i].NID_CDB=pop(buffer, 15, offset);
+		offset += 32;
+		data.pkg.pstato1[i].Q_STATOCDB=pop(buffer, 2, offset);
+		offset += 2;
+		data.pkg.pstato1[i].Q_STATOCDB=pop(buffer, 2, offset);
+		offset += 2;
+	}
+
+	
+}
--- Prototipo/pacchettostatolineaatc.h
+++ Prototipo/pacchettostatolineaatc.h
@@ -7,16 +7,15 @@ public:
 	pacchettostatolineaatc();
 	messtatolinea data;
 
-	void serializeStructuredHeader(byte *buff, messtatolinea &h, int &index);
-	void deserializeStructuredHeader(byte *buff, messtatolinea &h, int &index);
+	
 	
 public:
 	// funzione che restituisce la dimensione (ideale, non quella dovuta agli allineamenti 
 	// fatti dal compilatore) in byte del messaggio tenendo anche in conto l'eventuale padding
 	// questa funzione sarà chiamata da chi vorrà serializzare il messaggio, per poter allocare il buffer
 	int getSize();
-	void serializeStructuredHeader(byte *buff, int &index){serializeStructuredHeader(buff, data, index);};
-	void deserializeStructuredHeader(byte *buff, int &index){deserializeStructuredHeader(buff, data, index);};
+	void serialize(byte *buff);
+	void deserialize(byte *buff);
 	
 	// funzioni di interfaccia set e get per ogni campo dati del pacchetto
 	void setNID_MESSAGE(int N){data.head.NID_MESSAGE = N;};
@@ -29,5 +28,19 @@ public:
 	int getNID_PACKET(){return data.pkg.NID_PACKET;};
 	void setL_PACKET(int L){data.pkg.L_PACKET = L;};
 	int getL_PACKET(){return data.pkg.L_PACKET;};
+
+	void setNID_OPERATIONAL(int NID){data.pkg.NID_OPERATIONAL = NID;};
+	int getNID_OPERATIONAL(){return data.pkg.NID_OPERATIONAL;};
+
+	void setN_ITER(int N);
+	int getN_ITER(){return data.pkg.N_ITER;};
+
+	void setNID_CDB(int index, int NID);
+	int getNID_CDB(int index);
+	void setQ_DEVIATIOIO(int index, int Q);
+	int getQ_DEVIATIOIO(int index);
+	void setQ_STATOCDB(int index, int Q);
+	int getQ_STATOCDB(int index);
+
 };
 
