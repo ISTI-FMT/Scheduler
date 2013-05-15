ab776f7e89562626e88f31d103cafc5060011f8f
Luca Cassano <cassano@isti.cnr.it>
2013-05-15 11:54:13 +0200

    Definita Classe Comando Blocco

---
+++ Prototipo/messaggi/pacchettoComandoBlocco.cpp
@@ -0,0 +1,38 @@
+#include "pacchettoComandoBlocco.h"
+#include "..\\utility.h"
+
+pacchettoComandoBlocco::pacchettoComandoBlocco(void)
+{
+	data.NID_PACKET = 0;
+	data.L_PACKET = 0;
+	data.NID_BLOCCO = 0;
+	data.Q_CMDBLOCCO = 0;
+}
+
+// funzione che restituisce la dimensione (ideale, non quella dovuta agli allineamenti 
+// fatti dal compilatore) in Byte del messaggio tenendo anche in conto l'eventuale padding
+// questa funzione sarà chiamata da chi vorrà serializzare il messaggio, per poter allocare il buffer
+int pacchettoComandoBlocco::getSize()
+{
+	return 55;
+}
+
+void pacchettoComandoBlocco::serialize(byte *buffer)
+{
+	push(buffer, data.NID_PACKET, 8, 51);
+	setL_PACKET(getSize());
+	push(buffer, data.L_PACKET, 13, 59);
+	push(buffer, data.NID_BLOCCO, 32, 72);
+	push(buffer, data.Q_CMDBLOCCO, 2, 104);}
+
+void pacchettoComandoBlocco::deserialize(byte *buffer)
+{
+	data.NID_PACKET=pop(buffer,  8, 51);
+	data.L_PACKET=pop(buffer, 13, 59);
+	data.NID_BLOCCO=pop(buffer, 32, 72);
+	data.Q_CMDBLOCCO=pop(buffer, 2, 104);
+}
+
+pacchettoComandoBlocco::~pacchettoComandoBlocco(void)
+{
+}
---
+++ Prototipo/messaggi/pacchettoComandoBlocco.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "struttureDatiMessaggi.h"
+
+/*-----------------------------------------------------------------------------------------------
+Alessio:
+L'ATS invia all'IXl messaggi contenenti comandi sui blocchi
+-------------------------------------------------------------------------------------------------*/
+
+class pacchettoComandoBlocco
+{
+	ComandoBlocco data;
+public:
+	pacchettoComandoBlocco(void);
+
+	void setNID_PACKET(int N){data.NID_PACKET = N;};
+	int getNID_PACKET(){return data.NID_PACKET;};
+	void setL_PACKET(int L){data.L_PACKET = L;};
+	int getL_PACKET(){return data.L_PACKET;};
+	void setNID_BLOCCO(int N){data.NID_BLOCCO = N;};
+	int getNID_BLOCCO(){return data.NID_BLOCCO;};
+	void setQ_CMDBLOCCO(int Q){data.Q_CMDBLOCCO = Q;};
+	int getQ_CMDBLOCCO(){return data.Q_CMDBLOCCO;};
+
+	// funzione che restituisce la dimensione (ideale, non quella dovuta agli allineamenti 
+	// fatti dal compilatore) in Byte del messaggio tenendo anche in conto l'eventuale padding
+	// questa funzione sarà chiamata da chi vorrà serializzare il messaggio, per poter allocare il buffer
+	int getSize();
+	void serialize(byte *buffer);
+	void deserialize(byte *buffer);
+
+	~pacchettoComandoBlocco(void);
+};
+
