b2e0c3d33fdfdf9e54eb15a1ab6e36feffeb79e2
Giorgio <imatesiu@gmail.com>
2013-05-28 13:07:58 +0200

    Nuova versione tabella orario con inclusi gli itinerarii di entrata ed uscita

--- Prototipo/form/SchedulerForm.h
+++ Prototipo/form/SchedulerForm.h
@@ -332,17 +332,20 @@ namespace Prototipo {
 					 form->PerformLayout();
 					 form->SuspendLayout();
 
-					 dataGridView1->ColumnCount = 7;
+					 dataGridView1->ColumnCount = 10;
 					 dataGridView1->RowCount = tabella->get_TabellaOrario()->Count*8;
 					 int colonna=0;
 					 int riga=0;
 					 dataGridView1->Columns[ 0 ]->Name = "Id_Treno";
-					 dataGridView1->Columns[ 1 ]->Name = "None Stazione";
-					 dataGridView1->Columns[ 2 ]->Name = "Orario Arrivo";
-					 dataGridView1->Columns[ 3 ]->Name = "Orario Partenza";
-					 dataGridView1->Columns[ 4 ]->Name = "BinProg";
-					 dataGridView1->Columns[ 5 ]->Name = "TempoMAPorte";
-					 dataGridView1->Columns[ 6 ]->Name = "LatoAPorte";
+					 dataGridView1->Columns[ 1 ]->Name = "Nome Stazione";
+					 dataGridView1->Columns[ 2 ]->Name = "id Stazione";
+					 dataGridView1->Columns[ 3 ]->Name = "Orario Arrivo";
+					 dataGridView1->Columns[ 4 ]->Name = "Orario Partenza";
+					 dataGridView1->Columns[ 5 ]->Name = "BinProg";
+					 dataGridView1->Columns[ 6 ]->Name = "TempoMAPorte";
+					 dataGridView1->Columns[ 7 ]->Name = "LatoAPorte";
+					 dataGridView1->Columns[ 8 ]->Name = "It Entrata";
+					 dataGridView1->Columns[ 9 ]->Name = "It Uscita";
 
 					 for each( KeyValuePair<int , List<Fermata^>^> kvp in tabella->get_TabellaOrario() )
 					 {
@@ -352,15 +355,18 @@ namespace Prototipo {
 
 
 						 for each (Fermata ^dvar in kvp.Value)
-						 {
-							 dataGridView1->Rows[riga]->Cells[1]->Value=dvar->getIdStazione();
+						 { 
+							 dataGridView1->Rows[riga]->Cells[1]->Value=dvar->getnameStazione();
+							 dataGridView1->Rows[riga]->Cells[2]->Value=dvar->getIdStazione();
 							 TimeSpan Arrivo = TimeSpan::FromSeconds(dvar->getOrarioArrivo()*30);
-							 dataGridView1->Rows[riga]->Cells[2]->Value= Arrivo.ToString();
+							 dataGridView1->Rows[riga]->Cells[3]->Value= Arrivo.ToString();
 							 TimeSpan Partenza = TimeSpan::FromSeconds(dvar->getOrarioPartenza()*30);
-							 dataGridView1->Rows[riga]->Cells[3]->Value=Partenza.ToString();
-							 dataGridView1->Rows[riga]->Cells[4]->Value=dvar->getBinarioProgrammato();
-							 dataGridView1->Rows[riga]->Cells[5]->Value=dvar->gettempoMinimoAperturaPorte();
-							 dataGridView1->Rows[riga]->Cells[6]->Value=dvar->getLatoAperturaPorte();
+							 dataGridView1->Rows[riga]->Cells[4]->Value=Partenza.ToString();
+							 dataGridView1->Rows[riga]->Cells[5]->Value=dvar->getBinarioProgrammato();
+							 dataGridView1->Rows[riga]->Cells[6]->Value=dvar->gettempoMinimoAperturaPorte();
+							 dataGridView1->Rows[riga]->Cells[7]->Value=dvar->getLatoAperturaPorte();
+							 dataGridView1->Rows[riga]->Cells[8]->Value=dvar->getnameitinerarioEntrata();
+							 dataGridView1->Rows[riga]->Cells[9]->Value=dvar->getnameitinerarioUscita();
 							 riga++;
 						 }
 
