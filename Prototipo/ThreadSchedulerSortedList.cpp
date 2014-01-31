a81b61c5eecfdf3ce2734f863ee58aa4626cabd4
Giorgio <imatesiu@gmail.com>
2014-01-31 10:40:26 +0100

    simulazione di due treni in sequenza ad ecm il 30 gennaio 2014

--- Prototipo/ThreadSchedulerSortedList.cpp
+++ Prototipo/ThreadSchedulerSortedList.cpp
@@ -34,7 +34,7 @@ ThreadSchedulerSortedList::ThreadSchedulerSortedList(EventQueue<StateCDB^> ^E0,E
 	wdogs=w;
 	managerATC=manATC;
 	managerIXL=manIXL;
-	ipixl="127.0.0.1";
+	ipixl="192.168.1.58";//"127.0.0.1";
 	RaccoltaTrenoRequestCDB  = gcnew Dictionary<Train^,List<int>^> ();
 	EQueueCambioOrario = gcnew EventQueue<List<Fermata^>^>();
 	_shouldStop=false;
--- Script/LanciaTreni-1.bat
+++ Script/LanciaTreni-1.bat
@@ -1,4 +1,7 @@
 @echo on
 Title FinestraTReno
-START  python simulaTreno.py Treno65281percorso1155.txt 0
+
+START  python simulaTreno.py Treno65315percorso1153.txt 0
+ping 127.0.0.1 -n 5 > nul
+START  python simulaTreno.py Treno65280percorso1152.txt 0
 
