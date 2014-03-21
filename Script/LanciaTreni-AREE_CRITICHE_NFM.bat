a362e011d6f96ccc12dbe1042df3fd45becdb29a
Giorgio <imatesiu@gmail.com>
2014-03-21 15:50:09 +0100

    nuovi test per simulazioni in aree critiche

---
+++ Script/LanciaTreni-AREE_CRITICHE_NFM.bat
@@ -0,0 +1,11 @@
+@echo on
+Title FinestraTReno
+START  python simulaRBC.py 
+ping 127.0.0.1 -n 5 > nul 
+START  python simulaTreno.py Treno7327percorso5321.txt 0
+ping 127.0.0.1 -n 5 > nul
+START  python simulaTreno.py Treno7237percorso5312.txt 0
+ping 127.0.0.1 -n 5 > nul
+START  python simulaTreno.py Treno7723percorso5132.txt 0
+ping 127.0.0.1 -n 5 > nul
+START  python simulaTreno.py Treno7732percorso5123.txt 0
