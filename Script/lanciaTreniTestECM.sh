8fbb68b0bf78e9b5c31c80eed2945a088f08119e
Giorgio <imatesiu@gmail.com>
2014-11-10 17:09:51 +0100

    add new scenari test

---
+++ Script/lanciaTreniTestECM.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaRBC2.py"'
+
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno65315percorso5151.txt 0"'
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno65281percorso1153.txt 0"'
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno65000percorso1152.txt 0"'
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno65280percorso1177.txt 0"'
+
+osascript -e 'tell application "Terminal" to do script "/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno65666percorso1154.txt 0"'
+
+
+#open -n -a Terminal.app --args /Users/isiu/github/Scheduler/Script/simulaRBC2.py
+#open -n -a Terminal.app --args '/Users/isiu/github/Scheduler/Script/simulaTreno2.py /Users/isiu/github/Scheduler/Script/Treno7327percorso5321.txt 0'
